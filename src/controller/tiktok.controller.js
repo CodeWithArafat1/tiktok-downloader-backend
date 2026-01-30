89fdd6450765d9af38eafc6f15c67d77f888af5d
CodeWithArafat1 <arafatnill2.0@gmail.com>
2026-01-30 06:09:01 +0600

    change url

--- src/controller/tiktok.controller.js
+++ src/controller/tiktok.controller.js
@@ -2,6 +2,10 @@
 import axios from "axios";
 
 export const downloadVideo = async (req, res) => {
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    res.setHeader('Surrogate-Control', 'no-store');
   try {
     const { url } = req.query;
 
